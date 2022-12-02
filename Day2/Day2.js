901d219a9f34b3c258401c9af1b0fce2759298d9
Félix Noël <felix.noel@avonnicreator.com>
2022-12-02 16:17:33 -0500

    day2 progress

--- Day2/Day2.js
+++ Day2/Day2.js
@@ -1,9 +1,20 @@
 import readFile from "../utils/readFiles.js";
 
+const RockPaperScissorsMap = {
+  A: "rock",
+  B: "paper",
+  C: "scissors",
+  X: "rock",
+  Y: "paper",
+  Z: "scissors",
+};
+const PointMap = { A: 1, B: 2, C: 3, X: 1, Y: 2, Z: 3 };
+const BeatsWho = { rock: "scissors", paper: "rock", scissors: "paper" };
+
 function parseInput(input) {
   input = input.split("\r\n");
-  const data = input.map((group) => {
-    return group.split(" ");
+  let data = input.map((group) => {
+    return group.split(" ").map((item) => RockPaperScissorsMap[item]);
   });
   return data;
 }
@@ -13,8 +24,10 @@ async function main() {
   const data = parseInput(input);
   console.log(data);
 
-  
-
+  data.forEach(pair => {
+    
+    
+  });
 
 }
 
